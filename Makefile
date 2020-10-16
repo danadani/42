11d5cd932bf9baadb77c3c3a74d28beb29ac5734
Haedan Lee <haelee@c2r10s6.42seoul.kr>
2020-10-16 23:06:11 +0900

    libft

--- Makefile
+++ Makefile
@@ -6,20 +6,25 @@
 #    By: haelee <haelee@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/10/08 17:19:08 by haelee            #+#    #+#              #
-#    Updated: 2020/10/08 17:41:23 by haelee           ###   ########.fr        #
+#    Updated: 2020/10/16 22:46:23 by haelee           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
-Name = libft.a
+NAME = libft.a
 
-SRCS = ft_strlen.c ft_strlcpy.c
+SRCS = ft_atoi.c ft_isalpha.c ft_memccpy.c ft_memmove.c ft_strlcat.c ft_strnstr.c \
+	   ft_bzero.c ft_isascii.c ft_memchr.c ft_memset.c ft_strlcpy.c ft_strrchr.c \
+	   ft_calloc.c ft_isdigit.c ft_memcmp.c ft_strchr.c ft_strlen.c ft_tolower.c \
+	   ft_isalnum.c ft_isprint.c ft_memcpy.c ft_strdup.c ft_strncmp.c ft_toupper.c \
+
 OBJS = $(SRCS:.c=.o)
-all : $(Name)
 
-$(Name) : $(OBJS)
-	ar cr $(Name) $(OBJS)
+all : $(NAME)
+
+$(NAME) : $(OBJS)
+	ar cr $(NAME) $(OBJS)
 
 $(OBJS) : $(SRCS)
 	$(CC) $(CFLAGS) $(SRCS)
@@ -28,6 +33,8 @@ clean :
 	rm -f $(OBJS)
 
 fclean : clean
-	rm -f $(name)
+	rm -f $(NAME)
 
-re : fclean all
+re :fclean all
+
+.PHONY : all clean fclean re
--- ft_atoi.c
+++ ft_atoi.c
@@ -1,22 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haelee <haelee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/16 22:53:35 by haelee            #+#    #+#             */
+/*   Updated: 2020/10/16 22:55:37 by haelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	unsigned int i;
-	int sign;
-	int num;
+	unsigned int	i;
+	int				sign;
+	int				num;
 
 	i = 0;
 	sign = 1;
 	num = 0;
-	while((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
 			sign = -1;
 		i++;
-	}		
+	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		if (num < 0 && sign < 0)
