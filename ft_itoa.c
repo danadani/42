4d1ea0884bcdc7efa470c9e2a8e0602adc9edb35
danadani <72374653+danadani@users.noreply.github.com>
2020-11-28 17:47:28 +0900

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -19,16 +19,15 @@ static size_t	get_num_len(int n)
 
 char			*ft_itoa(int n)
 {
-	size_t		n_len;
+	size_t		numlen;
 	size_t		sign;
 	char		*result;
 	long long	n1;
 
-	n_len = get_num_len(n);
-	result = (char *)malloc(sizeof(char) * (n_len + 1));
-	if (result == NULL)
+	numlen = get_num_len(n);
+	if (!(result = (char *)malloc(sizeof(char) * (numlen + 1))))
 		return (NULL);
-	result[n_len] = '\0';
+	result[numlen] = '\0';
 	sign = 0;
 	n1 = (long long)n;
 	if (n1 < 0)
@@ -37,9 +36,9 @@ char			*ft_itoa(int n)
 		result[0] = '-';
 		sign = 1;
 	}
-	while (n_len-- > sign)
+	while (numlen-- > sign)
 	{
-		result[n_len] = (n1 % 10) + '0';
+		result[numlen] = (n1 % 10) + '0';
 		n1 /= 10;
 	}
 	return (result);
